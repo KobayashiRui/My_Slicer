2a7dd0c5428bd9c8b293773d42f549e3f746f1a8
Kobayashi Rui <roboroborx782@gmail.com>
2020-04-18 17:58:52 +0900

    remove slicer

---
+++ Slicer_2D/build/CMakeFiles/slicer.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rui/Documents/GitHub/My_Slicer/slicer/main.cpp" "/Users/rui/Documents/GitHub/My_Slicer/slicer/build/CMakeFiles/slicer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././stl_read"
+  ".././matplotlib"
+  "/usr/local/Cellar/python/3.7.4_1/Frameworks/Python.framework/Versions/3.7/include/python3.7m"
+  "/usr/local/lib/python3.7/site-packages/numpy/core/include"
+  "/usr/local/Cellar/eigen/3.3.7/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/rui/Documents/GitHub/My_Slicer/slicer/build/stl_read/CMakeFiles/stl_read.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
